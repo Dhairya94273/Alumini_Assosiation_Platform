4494d6c5af37fe342fe7b47bda3ee4fbb3adc632
Dhairya94273 <shahdhairya712@gmail.com>
2026-02-20 21:14:36 +0530

    Change Database to railway

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,10 @@ const connection = mysql.createConnection({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((err) => {
