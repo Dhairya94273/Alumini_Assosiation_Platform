8af2caba0b28e72c626ca189e130fb282565d90e
Dhairya94273 <shahdhairya712@gmail.com>
2026-02-20 22:13:02 +0530

    change in connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,15 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection(process.env.MYSQL_URL);
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 connection.connect((err) => {
   if (err) {
